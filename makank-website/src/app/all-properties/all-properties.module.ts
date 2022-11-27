5d3b138ddd74d8e0b4732076a8f1fcbcf8a22e77
MarkEhab33 <markoehab33@gmail.com>
2022-11-27 19:54:54 +0200

    Nav-bar

---
+++ makank-website/src/app/all-properties/all-properties.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertiesComponent } from './properties/properties.component';
+
+
+
+@NgModule({
+  declarations: [
+    PropertiesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AllPropertiesModule { }
