fcb018e6a7d52baafd0b6c51b0250998af85d886
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2022-11-29 17:54:25 +0200

    inital classes

--- makank-website/src/app/all-properties/properties/properties.component.ts
+++ makank-website/src/app/all-properties/properties/properties.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Post } from 'src/app/shared/post';
+import { Apartment, Property, Villa } from 'src/app/shared/property';
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.css']
+  styleUrls: ['./properties.component.css' ]
 })
 export class PropertiesComponent implements OnInit {
 
@@ -12,4 +13,23 @@ export class PropertiesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getDummyPost():Post{
+    let p = new Post()
+    p.postID=5
+    p.publishDate=new Date()
+    p.property=new Property()
+    p.property.pictures=['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsleUSqV4jrTGyQ-LfR3V5bkobZGtG0hyXf5ObJEy7&s']
+    p.property.area=25
+    p.property.city='ka3 el hamoor'
+    p.property.info='pinapple 3 levels very good neighborhood\n neighbors are jazz playning squid and a pink starfish'
+    p.property.bathroomNumber=1
+    p.property.rent=false
+    p.property.roomNumber=4
+    p.property.sellerID=145
+    p.property.price=50000000000
+    return p
+  }
+
+
 }
+
---
+++ makank-website/src/app/shared/property.ts
@@ -0,0 +1,23 @@
+export class Property{
+    sellerID: number = 0
+    roomNumber:number = 0
+    bathroomNumber:number = 0
+    price:number = 0 
+    city: string=""
+    area:number=0
+    rent:boolean=false
+    info:String=""
+    pictures:string[] = []
+}
+
+export class Apartment extends Property{
+    level: number=1
+    elevator: boolean= false
+    studentHousing: boolean= false 
+}
+
+export class Villa extends Property{
+    hasGarden:boolean=false
+    hasPool:boolean=false 
+    numberOfLevels:number=1;
+}
