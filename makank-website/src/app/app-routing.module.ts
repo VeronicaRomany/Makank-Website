96ce5756b58d9bae1b2ed7a49389760749c1829c
Peter <peteryasser80@gmail.com>
2022-11-30 21:00:50 +0200

    Login & Cookies without review

--- makank-website/src/app/app-routing.module.ts
+++ makank-website/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 
-
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
