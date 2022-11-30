3feef3686714abb9deb5431e95adf46d04ea6b45
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-11-30 23:00:50 +0200

    Front end registration form

--- makank-website/src/app/app.module.ts
+++ makank-website/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { EventEmitter, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './registration/register/register.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
