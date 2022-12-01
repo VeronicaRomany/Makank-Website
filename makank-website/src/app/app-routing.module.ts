79033d54bcf409619eadbcb4621da90f6568822e
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-12-01 19:33:01 +0200

    Revert "Merge branch 'Milestone-1' into registration"
    This reverts commit 5f7c2595abbba16edddfe632fe1e56b5b615874e, reversing
    changes made to 3feef3686714abb9deb5431e95adf46d04ea6b45.

--- makank-website/src/app/app-routing.module.ts
+++ makank-website/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PropertiesComponent } from './all-properties/properties/properties.component';
-import { LoginComponent } from './login/login/login.component';
-import { ProfileComponent } from './my-profile/profile/profile.component';
-import { HeaderComponent } from './shared/header/header.component';
 
-const routes: Routes = [
-{path:"Home" , component:PropertiesComponent},
-{path:"Profile" , component:ProfileComponent},
-{path:"Login" , component:LoginComponent},
-{path:"**" ,redirectTo:"Home",pathMatch:"full"}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
