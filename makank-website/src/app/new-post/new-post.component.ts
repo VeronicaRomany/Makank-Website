2f54e5835f3d9182044cc9cb0b769da463d11807
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-12-26 23:04:00 +0200

    change radio buttons to check boxes

--- makank-website/src/app/new-post/new-post.component.ts
+++ makank-website/src/app/new-post/new-post.component.ts
@@ -2,13 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
-import Validation from '../registration/register/register.component';
 import { Post } from '../shared/post';
 import { NewPostService } from './service/new-post.service';
 import { Globals } from 'src/globals';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TokenStorageService } from '../login/services/token-storage.service';
-import { ThrowStmt } from '@angular/compiler';
+
 
 
 
@@ -19,7 +18,7 @@ import { ThrowStmt } from '@angular/compiler';
   providers: [ Globals ],
 })
 export class NewPostComponent implements OnInit {
-  [x: string]: any;
+  
 
   newPostForm: FormGroup = new FormGroup({
     type: new FormControl(''),
@@ -42,18 +41,11 @@ export class NewPostComponent implements OnInit {
   submitted = false;
   villa = false;
   appartment = false;
- 
-  alreadyCheckedElevator=false;
-  alreadyCheckedPool=false;
-  alreadCheckedGarden=false;
-  alreadyCheckedStudent=false;
   photosLinks :string[]=[]
   index =0;
-  file: any;
-
   editing=false
+  file: any;
   event:any
-
   newPost : Post= new Post()
   editPost : Post=new Post()
 
@@ -81,20 +73,18 @@ export class NewPostComponent implements OnInit {
         studentHousing: [ '', ''],
         pool: [ '', ''],
         garden: [ '', ''],
-        universities :[ '', '']
       },
     );
 
 
     this.route.queryParams.subscribe((params:any) =>{
       console.log(params.data)
-
       if(params.data!=null){
             this.editing=true
             console.log("edit post with id = ",params.data);
-            this. editPost.postID=params.data
+            this.editPost.postID=params.data
             
-            // get post by id
+            // get post by id   >> request lma a3mlo ams7 al dummy data de
       
           
             //dummy data
@@ -106,37 +96,21 @@ export class NewPostComponent implements OnInit {
             this .editPost.level=12
             this. editPost.roomNumber=12
             this. editPost.bathroomNumber=12
-            this. editPost.elevator=false
-            this. editPost.studentHousing=false
+            this. editPost.elevator=true
+            this. editPost.studentHousing=true
             this. editPost.hasGarden=true
             this. editPost.hasPool=true
             this. editPost.info=""
-            this. editPost.universities=[]
-            this.editPost.rent=true
-            this.editPost.pictures=[]
+            this. editPost.rent=true
+            this. editPost.pictures=[]
       
+
+            // nms7 l7d hena
             if(this.editPost.rent){
               this. newPostForm.controls["RentOrBuy"].setValue("rent")
             }else{
               this. newPostForm.controls["RentOrBuy"].setValue("buy")
             }
-      
-            if(this.editPost.studentHousing){
-              this.alreadyCheckedStudent=true
-            }
-      
-            if(this.editPost.elevator){
-              this.alreadyCheckedElevator=true
-            }
-      
-            if(this.editPost.hasGarden){
-              this.alreadCheckedGarden=true;
-            }
-      
-            if(this.editPost.hasPool){
-              this.alreadyCheckedPool=true
-            }
-      
       
             if(this.editPost.hasPictures){
               for (let i = 0; i < this.editPost.pictures.length; i++) {
@@ -144,8 +118,6 @@ export class NewPostComponent implements OnInit {
             }
           }
       
-      
-        
             this. newPostForm.controls["type"].setValue(this.editPost.type)
             this.onChooseType()
             this. newPostForm.controls["city"].setValue(this.editPost.city)
@@ -156,13 +128,8 @@ export class NewPostComponent implements OnInit {
             this. newPostForm.controls["WCNum"].setValue(this.editPost.bathroomNumber)
             this. newPostForm.controls["address"].setValue(this.editPost.address)
             this. newPostForm.controls["info"].setValue(this.editPost.info)
-            this. newPostForm.controls["universities"].setValue(this.editPost.universities)
             
-      
-
-      }
-     
-    })
+      } })
   }
 
 
@@ -173,7 +140,6 @@ export class NewPostComponent implements OnInit {
 
 
   onSubmit(): void {
-    
     this.submitted = true;
     if (this.newPostForm.invalid) {
       return;
@@ -181,9 +147,9 @@ export class NewPostComponent implements OnInit {
 
   
 
-    console.log("veroooooooooooo creates new Post")
+    console.log("create new post")
 
-    //fadel sellerID
+   
     this.newPost.type = this.f['type'].value
     this.newPost.city = this.f['city'].value
     this.newPost.price= this.f['price'].value
@@ -192,13 +158,24 @@ export class NewPostComponent implements OnInit {
     this.newPost.level=this.f['level'].value
     this.newPost.roomNumber=this.f['roomsNum'].value
     this.newPost.bathroomNumber=this.f['WCNum'].value
-    this.newPost.elevator=this.alreadyCheckedElevator
-    this.newPost.studentHousing=this.alreadyCheckedStudent
-    this.newPost.hasGarden=this.alreadCheckedGarden
-    this.newPost.hasPool=this.alreadyCheckedPool
     this.newPost.info=this.f['info'].value
-    this.newPost.universities=this.f['universities'].value
     this.newPost.sellerID=this.token.getUser().userId;
+
+    if(this.villa){
+      this.newPost.hasGarden=(document.getElementById("garden") as HTMLInputElement).checked
+      this.newPost.hasPool=(document.getElementById("pool") as HTMLInputElement).checked
+      this.newPost.elevator=false
+      this.newPost.studentHousing=false
+    }
+
+    if(this.appartment){
+      this.newPost.elevator=(document.getElementById("elev") as HTMLInputElement).checked
+      this.newPost.studentHousing=(document.getElementById("student") as HTMLInputElement).checked
+      this.newPost.hasGarden=false
+      this.newPost.hasPool=false
+    }
+
+
  
 
     if(this.f['RentOrBuy'].value=="rent"){
@@ -220,34 +197,27 @@ export class NewPostComponent implements OnInit {
     console.log(NewPostJsonString)
     this.http.post("http://localhost:8080/posts/new",JSON.parse(NewPostJsonString),{responseType:'text'}).subscribe((data:any) =>{
       this.router.navigate(['/', 'Home'])
-      
     })
   }
 
 
   onEdit():void{
-
     this.newPost.postID=this.editPost.postID
     console.log( this.newPost.postID)
     this.onSubmit()
   }
 
   onChooseType(): void{
-    console.log(this.f['type'].value)
-    if(this.f['type'].value=="villa"){
-        this.villa = true
-        this.appartment= false
-        this.alreadyCheckedElevator=false
-        this.alreadyCheckedStudent=false
-    }
+        console.log(this.f['type'].value)
+        if(this.f['type'].value=="villa"){
+            this.villa = true
+            this.appartment= false;
+        }
 
-    if(this.f['type'].value=="apartment"){
-      this.appartment= true
-      this.villa= false
-      this.alreadCheckedGarden=false
-      this.alreadyCheckedPool=false
-  }
-  
+        if(this.f['type'].value=="apartment"){
+          this.appartment= true
+          this.villa= false;
+      }
   }
 
 
@@ -256,10 +226,6 @@ export class NewPostComponent implements OnInit {
     this.submitted = false;
     this.appartment = false;
     this.villa = false;
-    this.alreadCheckedGarden=false;
-    this.alreadyCheckedElevator=false;
-    this.alreadyCheckedPool=false;
-    this.alreadyCheckedStudent=false;
     this.photosLinks=[];
     this.newPostForm.reset();
   }
@@ -268,78 +234,14 @@ export class NewPostComponent implements OnInit {
   RemoveMe(index:number){
     console.log(index)
      this.photosLinks.splice(index,1)
-    console.log(this.photosLinks)
+     console.log(this.photosLinks)
   }
-
-  handleChangeElevator(event:any) {
-    var target = event.target;
-    if(this.alreadyCheckedElevator){
-      target.checked=false
-      this.alreadyCheckedElevator=!this.alreadyCheckedElevator
-      console.log(this.alreadyCheckedElevator)
-      console.log(" not Checked")
-    }else{
-      target.checked=true;
-      this.alreadyCheckedElevator=!this.alreadyCheckedElevator
-      console.log(this.alreadyCheckedElevator)
-      console.log("Checked")
-    }
-  }
-
-  handleChangeStudent(event:any) {
-    var target = event.target;
-    if(this.alreadyCheckedStudent){
-      target.checked=false
-      this.alreadyCheckedStudent=!this.alreadyCheckedStudent
-      console.log(this.alreadyCheckedStudent)
-      console.log(" not Checked")
-    }else{
-      target.checked=true;
-      this.alreadyCheckedStudent=!this.alreadyCheckedStudent
-      console.log(this.alreadyCheckedStudent)
-      console.log("Checked")
-    }
-  }
-
-
-  handleChangePool(event:any) {
-    var target = event.target;
-    if(this.alreadyCheckedPool){
-      target.checked=false
-      this.alreadyCheckedPool=!this.alreadyCheckedPool
-      console.log(this.alreadyCheckedPool)
-      console.log(" not Checked")
-    }else{
-      target.checked=true;
-      this.alreadyCheckedPool=!this.alreadyCheckedPool
-      console.log(this.alreadyCheckedPool)
-      console.log("Checked")
-    }
-  }
-
-  handleChangeGarden(event:any) {
-    var target = event.target;
-    if(this.alreadCheckedGarden){
-      target.checked=false
-      this.alreadCheckedGarden=!this.alreadCheckedGarden
-      console.log(this.alreadCheckedGarden)
-      console.log(" not Checked")
-    }else{
-      target.checked=true;
-      this.alreadCheckedGarden=!this.alreadCheckedGarden
-      console.log(this.alreadCheckedGarden)
-      console.log("Checked")
-    }
-  }
-
-  
 
 
   SelectFile(event:any){
     this.file = event.target.files[0];
     this.newpostservice.upload(this.file).subscribe((url: string) => this. photosLinks[this.index++]=url )
-    
-    
   }
 
 }
+
