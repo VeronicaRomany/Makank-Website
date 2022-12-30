ebd32cf14d55dff2c6820334f7631464d4aada7f
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2022-12-30 15:03:57 +0200

    Update PostService.java

--- mkanak_spring/src/main/java/mkanak_spring/model/services/PostService.java
+++ mkanak_spring/src/main/java/mkanak_spring/model/services/PostService.java
@@ -13,11 +13,8 @@ public interface PostService {
     List<Post> getHomepagePosts(JSONObject preference,int pageNum,int pageSize) ;
     long getHomepagePostsCount(JSONObject preference) ;
 
-    List<Post> getSavedPosts(int id, JSONObject preference,int pageNum,int pageSize);
-    List<Post> getProfilePosts(int targetUserID, JSONObject preferences, int pageNum,int pageSize);
     void addToSavedPosts(JSONObject entry);
     void removeFromSaved(JSONObject entry);
-    void editPost(JSONObject post) throws ParseException;
     JSONObject getPostDetails(long postID) throws ParseException;
     JSONObject getProperty(long propertyID) throws ParseException;
 
@@ -25,8 +22,6 @@ public interface PostService {
     List<Long> getSavedPostsIDs(int userID);
     long getSavedPostsCount(int id, JSONObject preferences);
 
-    void addToSavedPosts(JSONObject entry);
-    void removeFromSaved(JSONObject entry);
 
     List<Post> getProfilePosts(int targetUserID, JSONObject preferences, int pageNum,int pageSize);
     long getProfilePostsCount(int targetUserID, JSONObject preferences);
